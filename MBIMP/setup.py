6c381fd83ea441473d807ef3a5ffef029d91b11c
gfhtj <15623258617@163.com>
2024-08-18 15:27:43 +0800

    second commit

---
+++ MBIMP/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="Impute",
+    version="1.0",
+    description="Method for microbiome imputation",
+    author="sun",
+    author_email="15623258617@163.com",
+    py_modules=["GEMimp"]
+    )
